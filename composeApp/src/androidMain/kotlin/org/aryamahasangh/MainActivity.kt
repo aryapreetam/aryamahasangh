bf1f344c8d59929db6febf8e58ff92ad5828e6d5
Preetam Bhosle <preetb123@gmail.com>
2025-05-28 02:00:26 +0530

    added full screen support for android & ios

--- composeApp/src/androidMain/kotlin/org/aryamahasangh/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/aryamahasangh/MainActivity.kt
@@ -2,34 +2,47 @@ package org.aryamahasangh
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.vinceglb.filekit.core.FileKit
 import org.aryamahasangh.config.AndroidContextHolder
 import org.aryamahasangh.config.ConfigInitializer
 
+import android.graphics.Color as AndroidColor
+
+
+
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        
-        // Initialize Android context holder
-        AndroidContextHolder.init(this)
-        
-        // Initialize cross-platform configuration
-        ConfigInitializer.initializeBlocking()
-        
-        // Initialize FileKit
-        FileKit.init(this)
-        
-        setContent {
-            App()
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    enableEdgeToEdge(
+      navigationBarStyle = SystemBarStyle.light(
+        AndroidColor.TRANSPARENT,
+        AndroidColor.TRANSPARENT
+      )
+    )
+
+    // Initialize Android context holder
+    AndroidContextHolder.init(this)
+
+    // Initialize cross-platform configuration
+    ConfigInitializer.initializeBlocking()
+
+    // Initialize FileKit
+    FileKit.init(this)
+
+    setContent {
+      App()
     }
+  }
 }
 
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+  App()
 }
