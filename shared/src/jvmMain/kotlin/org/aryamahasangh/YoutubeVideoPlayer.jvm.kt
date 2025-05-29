3b73c8c245eb7d6d9197093b4f70a5b1d53b2707
Preetam Bhosle <preetb123@gmail.com>
2025-05-29 17:31:51 +0530

    fix issue where registration success snackbar appeared multiple times

--- shared/src/jvmMain/kotlin/org/aryamahasangh/YoutubeVideoPlayer.jvm.kt
+++ shared/src/jvmMain/kotlin/org/aryamahasangh/YoutubeVideoPlayer.jvm.kt
@@ -1,11 +1,7 @@
 package org.aryamahasangh
 
 import androidx.compose.runtime.Composable
-import com.multiplatform.webview.web.WebView
-import com.multiplatform.webview.web.rememberWebViewState
 
 @Composable
 actual fun YoutubeVideoPlayer(videoUrl: String) {
-  val state = rememberWebViewState("https://www.youtube.com/embed/$videoUrl")
-  WebView(state)
 }
