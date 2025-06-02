3d6d4ea25296e0a7700c79ab158208a4ea790c25
Preetam Bhosle <preetb123@gmail.com>
2025-06-02 14:15:48 +0530

    upgraded compose multiplatform version to 1.8.1

--- composeApp/src/commonTest/kotlin/org/aryamahasangh/SimpleTest.kt
+++ composeApp/src/commonTest/kotlin/org/aryamahasangh/SimpleTest.kt
@@ -1,11 +1,12 @@
 package org.aryamahasangh
 
-// import kotlin.test.Test
-// import kotlin.test.assertTrue
+ import kotlin.test.Test
+ import kotlin.test.assertTrue
 
 class SimpleTest {
-  // @Test
-  // fun testBasicFunctionality() {
-  //   assertTrue(true, "Basic test should pass")
-  // }
+   @Test
+   fun testBasicFunctionality() {
+     println("executing test")
+     assertTrue(true, "Basic test should pass")
+   }
 }
